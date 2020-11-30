d75836694de5e7a3bbc76257fa5008ee747cdf4c
Viet <viet.dang@amagumolabs.com>
2020-11-30 22:53:33 +0700

    Create the burger

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import './App.css';
 
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,24 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const { ingredients } = props;
+    let transformedIngredients = Object.keys(ingredients)
+        .map(igKey => {
+            return [...Array(ingredients[igKey])]
+                .map((_, i) => (
+                    <BurgerIngredient key={igKey+i} type={igKey}/>
+                ))
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, [])
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <strong>Please start adding ingredients</strong>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='meat'/>
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom'/>
         </div>
     );
